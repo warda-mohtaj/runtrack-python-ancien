cdf841bf1505383be6c4bbe40fce29e11818b3bf
warda-mohtaj <115171252+warda-mohtaj@users.noreply.github.com>
2023-01-04 14:28:15 +0100

    Add files via upload

---
+++ Jour3/job3.py
@@ -0,0 +1,5 @@
+nombre = 0
+for nombre in range(101):
+    if nombre == 26 or nombre == 37 or nombre == 88:
+            continue
+    print(nombre)
---
+++ Jour3/job4.py
@@ -0,0 +1,7 @@
+for fizzbuzz in range(101):
+    if fizzbuzz%3==0:
+        print('Fizz')
+    elif fizzbuzz%5==0:
+        print('Buzz')
+    else:
+        print(fizzbuzz)
