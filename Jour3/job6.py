e43001de6173b21e2ac322435a629f4abccbad44
warda-mohtaj <115171252+warda-mohtaj@users.noreply.github.com>
2023-01-04 14:33:24 +0100

    Add files via upload

---
+++ Jour3/job6.py
@@ -0,0 +1,5 @@
+chaine="abcdefghijklmnopqrstuvwxyz" * 10 
+  
+for pyramide in range(1, int((-1 + (1 + 8*len(chaine))**0.5) // 2) + 1): 
+    print(chaine[:pyramide])  
+    chaine=chaine[pyramide:]
